4b7ff8e5837e89c475a3ed5b1d763ac8832e80c7
ellie <ellie.tms1371@gmail.com>
2020-08-01 23:22:28 +0100

    find rooms by Searching  by name or summary.

---
+++ server.js
@@ -0,0 +1,39 @@
+const Dotenv = require("dotenv");
+Dotenv.config();
+const express = require("express");
+const mongodb = require("mongodb");
+const URI = process.env.DATABASE_URI;
+const app = express();
+const client = new mongodb.MongoClient(URI,{ useUnifiedTopology: true });
+
+client.connect(() => {
+  const db = client.db("sample_airbnb");
+  const collection = db.collection("listingsAndReviews");
+
+  app.get("/search", (request, response) => {
+    const { name } = request.query;
+    const { summary } = request.query;
+    const searchObject = {
+        $or: [
+            {summary:summary},
+            {name:name},  
+        ],
+    }
+    collection.find(searchObject).toArray((error, results) => {
+      if (name !== "" || summary !== "") {
+        if(error){
+            response.status(500).send(error);
+        }
+        else{
+            response.send(results);
+        }
+      }
+      else{
+          response.send("Sorry please search again");
+      }
+    });
+  });
+});
+
+console.log("working");
+app.listen(3000);
